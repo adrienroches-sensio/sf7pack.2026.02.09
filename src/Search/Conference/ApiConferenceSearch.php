9c6a357bf3f1e13fd77ba0c1a53ff500ca64dc57
Adrien Roches <adrien@roc-it.tech>
2026-02-12 12:46:05 +0100

    [sf-advanced] Inject api key in api conference search

--- src/Search/Conference/ApiConferenceSearch.php
+++ src/Search/Conference/ApiConferenceSearch.php
@@ -4,8 +4,15 @@ declare(strict_types=1);
 
 namespace App\Search\Conference;
 
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
+
 final class ApiConferenceSearch implements ConferenceSearchInterface
 {
+    public function __construct(
+        #[Autowire(env: 'CONFERENCES_API_KEY')]
+        private readonly string $apiKey,
+    ) {
+    }
 
     public function searchByName(?string $name = null): array
     {

b7ab703a13f3e1254911584e1e93151f2afaa02f
Adrien Roches <adrien@roc-it.tech>
2026-02-12 15:03:53 +0100

    [sf-advanced] Define and use custom http client for devevents

--- src/Search/Conference/ApiConferenceSearch.php
+++ src/Search/Conference/ApiConferenceSearch.php
@@ -5,28 +5,21 @@ declare(strict_types=1);
 namespace App\Search\Conference;
 
 use Symfony\Component\DependencyInjection\Attribute\AsAlias;
-use Symfony\Component\DependencyInjection\Attribute\Autowire;
+use Symfony\Component\DependencyInjection\Attribute\Target;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 #[AsAlias(ConferenceSearchInterface::class)]
 final class ApiConferenceSearch implements ConferenceSearchInterface
 {
     public function __construct(
+        #[Target('conferences.client')]
         private readonly HttpClientInterface $client,
-
-        #[Autowire(env: 'CONFERENCES_API_KEY')]
-        private readonly string $apiKey,
     ) {
     }
 
     public function searchByName(?string $name = null): array
     {
-        $options = [
-            'headers' => [
-                'apikey' => $this->apiKey,
-                'accept' => 'application/json'
-            ],
-        ];
+        $options = [];
 
         $name = trim($name ?? '');
 
@@ -35,7 +28,7 @@ final class ApiConferenceSearch implements ConferenceSearchInterface
             $options['query']['name'] = $name;
         }
 
-        $response = $this->client->request('GET', 'https://devevents-api.fr/events', $options);
+        $response = $this->client->request('GET', '/events', $options);
         dump($response->toArray());
 
         return [];

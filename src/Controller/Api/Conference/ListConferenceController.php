4edd50b54180eb78684b6f0e3e97c1b9552ad679
Adrien Roches <adrien@roc-it.tech>
2026-02-13 15:29:43 +0100

    [sf-advanced] Handled circular reference with circular reference handler (leaf-first)

--- src/Controller/Api/Conference/ListConferenceController.php
+++ src/Controller/Api/Conference/ListConferenceController.php
@@ -7,6 +7,7 @@ namespace App\Controller\Api\Conference;
 use App\Search\Conference\ConferenceSearchInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 use Symfony\Component\Serializer\SerializerInterface;
 
 #[Route(
@@ -25,7 +26,11 @@ final class ListConferenceController
     public function __invoke(): JsonResponse
     {
         return new JsonResponse(
-            $this->serializer->serialize($this->conferenceSearch->searchByName(), 'json'),
+            $this->serializer->serialize($this->conferenceSearch->searchByName('202'), 'json', [
+                AbstractNormalizer::CIRCULAR_REFERENCE_HANDLER => function(object $object, string $format, array $context) {
+                    return $object->getId();
+                }
+            ]),
             json: true
         );
     }
